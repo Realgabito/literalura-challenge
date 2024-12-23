244822ed8ab77a109d7d80244471af30c85e4b28
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-22 23:30:38 -0500

    Create main package and class

---
+++ src/main/java/com/literalura/literalura/main/Main.java
@@ -0,0 +1,15 @@
+package com.literalura.literalura.main;
+
+import com.literalura.literalura.service.ConvertData;
+import com.literalura.literalura.service.RequestAPI;
+import java.util.Scanner;
+
+public class Main {
+    private RequestAPI requestAPI = new RequestAPI();
+    private ConvertData converter = new ConvertData();
+    private final String URL = "https://gutendex.com/books/";
+    private Scanner userInput = new Scanner(System.in);
+
+
+
+}
