f83ce5c2bf4cadae76c38d4b21ee4999e01a051f
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-22 23:31:41 -0500

    Update: implement CommandLineRunner in LiteraluraApplication and update AuthorData record

--- src/main/java/com/literalura/literalura/model/AuthorData.java
+++ src/main/java/com/literalura/literalura/model/AuthorData.java
@@ -6,6 +6,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record AuthorData(
         @JsonAlias("name") String name,
-        @JsonAlias("birth_year") String DoB
+        @JsonAlias("birth_year") String DoB,
+        @JsonAlias("death_year") String DoD
 ) {
+    @Override
+    public String toString() {
+        return  "Nombre: " + name + '\n' +
+                "Año de nacimiento: " + DoB + '\n' +
+                "Año de fallecimiento: " + DoD + '\n';
+    }
 }
