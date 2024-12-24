6fc7cbe304abee7e2071fac69c39e519c1700870
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-24 16:36:31 -0500

    Update: add @Autowired anotation for bookRepository

--- src/main/java/com/literalura/literalura/LiteraluraApplication.java
+++ src/main/java/com/literalura/literalura/LiteraluraApplication.java
@@ -1,6 +1,9 @@
 package com.literalura.literalura;
 
 import com.literalura.literalura.main.Main;
+import com.literalura.literalura.repository.AuthorRepository;
+import com.literalura.literalura.repository.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
+	@Autowired
+	private BooksRepository booksRepository;
+	private AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Main main = new Main();
+		Main main = new Main(booksRepository);
 		main.showMenu();
 	}
 }
