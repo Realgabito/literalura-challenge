5e03f965cd13fd2d87edbf8c0636b62d50dab910
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-23 20:02:16 -0500

    Add: Main class to create menu for user interaction

--- src/main/java/com/literalura/literalura/main/Main.java
+++ src/main/java/com/literalura/literalura/main/Main.java
@@ -1,5 +1,6 @@
 package com.literalura.literalura.main;
 
+import com.literalura.literalura.model.Data;
 import com.literalura.literalura.service.ConvertData;
 import com.literalura.literalura.service.RequestAPI;
 import java.util.Scanner;
@@ -11,5 +12,31 @@ public class Main {
     private Scanner userInput = new Scanner(System.in);
 
 
+    public void showMenu() {
+        var json = requestAPI.getData(URL);
+//        System.out.println(json);
 
+        var data = converter.getData(json, Data.class);
+        System.out.println(data);
+
+        var option = -1;
+        while  (option != 0) {
+            var menu = """
+                    Elija la opcion a traves de su numero:
+                    1 - Buscar libro por titulo
+                    2 - Listar todos los libros registrados
+                    3 - Listar autores registrados
+                    4 - Listar autores vivos en un determinado año
+                    5 - Listar libros por idioma
+                    
+                    0 - Salir
+                    """;
+
+            System.out.println(menu);
+            option = userInput.nextInt();
+            userInput.nextLine();
+
+
+        }
+    }
 }
