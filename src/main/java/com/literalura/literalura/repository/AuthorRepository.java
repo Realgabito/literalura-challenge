da378212dd32c4d1909bf91ce38e9e96d123dd3f
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-24 16:32:18 -0500

    FEAT: Create repository interface for Books data amd Author's data

--- src/main/java/com/literalura/literalura/repository/AuthorRepository.java
+++ src/main/java/com/literalura/literalura/repository/AuthorRepository.java
@@ -1,4 +1,7 @@
 package com.literalura.literalura.repository;
 
-public interface AuthorRepository {
+import com.literalura.literalura.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
--- src/main/java/com/literalura/literalura/repository/BooksRepository.java
+++ src/main/java/com/literalura/literalura/repository/BooksRepository.java
@@ -1,4 +1,7 @@
 package com.literalura.literalura.repository;
 
-public interface BooksRepository {
+import com.literalura.literalura.model.Books;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BooksRepository extends JpaRepository<Books, Long> {
 }
