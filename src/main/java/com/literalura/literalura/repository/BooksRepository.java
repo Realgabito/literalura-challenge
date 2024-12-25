333dd550de5096d5da1293fd13a27ec4fb3f6d26
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-25 17:11:11 -0500

    Add methods to BooksRepository for title and language searches\n\n - Add a method to find books by title using a partial matching with the case insensitivity.\n - Add a method to find books by language with the insentitive search.\n - Annotate interface with @Repository for consistency and clarity.

--- src/main/java/com/literalura/literalura/repository/BooksRepository.java
+++ src/main/java/com/literalura/literalura/repository/BooksRepository.java
@@ -2,6 +2,13 @@ package com.literalura.literalura.repository;
 
 import com.literalura.literalura.model.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface BooksRepository extends JpaRepository<Books, Long> {
+    Optional<Books> findByTitleContainingIgnoreCase(String bookTitle);
+    List<Books> findByLanguagesContainingIgnoreCase(String language);
 }
