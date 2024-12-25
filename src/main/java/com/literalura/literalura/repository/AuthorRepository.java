9480271606d24a2c7a16958118941efcb9264912
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-25 17:19:00 -0500

    Update: Enhance AuthorRepository with additional query methods.\n -Addded a method to find authors by name. \n -Added a method to find authors born before or dying after specific date \n - Introduce a custom query and annotated the repository with @Repository.

--- src/main/java/com/literalura/literalura/repository/AuthorRepository.java
+++ src/main/java/com/literalura/literalura/repository/AuthorRepository.java
@@ -2,6 +2,18 @@ package com.literalura.literalura.repository;
 
 import com.literalura.literalura.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByNameIgnoreCase(String name);
+    List<Author> findByDateOfBirthLessThanEqualAndDateOfDeathGreaterThanEqual(Integer dateOfBirth , Integer dateOfDeath);
+
+    @Query("SELECT a FROM Author a WHERE a.dateOfBirth <= :year AND (a.dateOfDeath >= :year OR a.dateOfDeath IS NULL)")
+    List<Author> findAliveInYear(@Param("year") int year);
 }
