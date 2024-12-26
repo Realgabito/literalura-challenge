535b0cf93c98d0d939d40381efa8f0121d0f0cfe
Victor Perdomo <victorgabperdomo@gmail.com>
2024-12-25 22:43:53 -0500

    Delete: remove unused file

--- src/main/java/com/literalura/literalura/service/notes.java
+++
@@ -1,5 +0,0 @@
-package com.literalura.literalura.service;
-
-public @interface notes {
-//    https://github.com/fjarar/literalura/blob/main/src/main/java/com/flaviojara/literalura/principal/Principal.java#L208
-}
